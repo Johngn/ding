a58d5fc26e8bebbb29b95b8cf6d714eb1553d2d0
John Gillan <jgillan@protonmail.com>
2021-06-19 22:43:29 +0100

    hid key

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -10,9 +10,7 @@ export const getData = () => {
     });
 
     try {
-      const { data } = await axios.get(
-        'https://app.fakejson.com/q/xdOdc9ZF?token=M37SFqOXjnZXOBpUuOCRXA__00'
-      );
+      const { data } = await axios.get('https://app.fakejson.com/q/__00');
 
       dispatch({
         type: ActionType.GET_API_DATA_SUCCESS,
